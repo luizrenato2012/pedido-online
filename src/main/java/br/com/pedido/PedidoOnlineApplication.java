8d5c3a7326cad373127790c4dad148b312fc6ef9
Luiz Renato <luizrenato2005@gmail.com>
2018-11-03 00:14:00 -0300

    commit inicial

---
+++ src/main/java/br/com/pedido/PedidoOnlineApplication.java
@@ -0,0 +1,12 @@
+package br.com.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoOnlineApplication.class, args);
+	}
+}
